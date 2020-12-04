1e630b3e4145e9c573b29571a51d1ef565a6e566
Danish <57083546+Danish-1@users.noreply.github.com>
2020-12-04 21:34:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+var i,j,k,x,y,z=0;
+function fn1(){
+    
+    if(!i){
+        document.getElementById("part11").style.display="inline";
+        document.getElementById("more1").innerHTML="READ LESS";
+        document.getElementById("part1").style.display="none";
+        i=1;
+    }
+    else{
+        document.getElementById("part11").style.display="none";
+        document.getElementById("more1").innerHTML="READ MORE";
+        document.getElementById("part1").style.display="inline";
+        i=0;
+    }
+}
+function fn2(){
+    
+    if(!j){
+        document.getElementById("part21").style.display="inline";
+        document.getElementById("more2").innerHTML="READ LESS";
+        document.getElementById("part2").style.display="none";
+        j=1;
+    }
+    else{
+        document.getElementById("part21").style.display="none";
+        document.getElementById("more2").innerHTML="READ MORE";
+        document.getElementById("part2").style.display="inline";
+        j=0;
+    }
+}
+function fn3(){
+    
+    if(!k){
+        document.getElementById("part31").style.display="inline";
+        document.getElementById("more3").innerHTML="READ LESS";
+        document.getElementById("part3").style.display="none";
+        k=1;
+    }
+    else{
+        document.getElementById("part31").style.display="none";
+        document.getElementById("more3").innerHTML="READ MORE";
+        document.getElementById("part3").style.display="inline";
+        k=0;
+    }
+}
+function fn4(){
+    
+    if(!x){
+        document.getElementById("part41").style.display="inline";
+        document.getElementById("more4").innerHTML="READ LESS";
+        document.getElementById("part4").style.display="none";
+        x=1;
+    }
+    else{
+        document.getElementById("part41").style.display="none";
+        document.getElementById("more4").innerHTML="READ MORE";
+        document.getElementById("part4").style.display="inline";
+        x=0;
+    }
+}
+function fn5(){
+    
+    if(!y){
+        document.getElementById("part51").style.display="inline";
+        document.getElementById("more5").innerHTML="READ LESS";
+        document.getElementById("part5").style.display="none";
+        y=1;
+    }
+    else{
+        document.getElementById("part51").style.display="none";
+        document.getElementById("more5").innerHTML="READ MORE";
+        document.getElementById("part5").style.display="inline";
+        y=0;
+    }
+}
+function fn6(){
+    
+    if(!z){
+        document.getElementById("part61").style.display="inline";
+        document.getElementById("more6").innerHTML="READ LESS";
+        document.getElementById("part6").style.display="none";
+        z=1;
+    }
+    else{
+        document.getElementById("part61").style.display="none";
+        document.getElementById("more6").innerHTML="READ MORE";
+        document.getElementById("part6").style.display="inline";
+        z=0;
+    }
+}
